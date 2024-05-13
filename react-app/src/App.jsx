76d445d45a9678f44c2fc61ff65ff110001d4a19
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-05-13 23:52:58 +0200

    created REACT-hw-9

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,1 @@
-import Collapse from "./components/Collapse.jsx";
-function App() {
 
-  return (
-      <>
-          <div className="App">
-              <Collapse text="Hello world!!!"/>
-          </div>
-      </>
-  )
-}
-
-export default App
