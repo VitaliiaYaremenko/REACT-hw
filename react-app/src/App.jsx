c89b3da00b740fa35c267edae5e182a5c660d93c
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-05-13 23:53:46 +0200

    added changes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,1 +1,13 @@
+import Collapse from "./components/Collapse.jsx";
+function App() {
 
+  return (
+      <>
+          <div className="App">
+              <Collapse text="Hello world!!!"/>
+          </div>
+      </>
+  )
+}
+
+export default App
